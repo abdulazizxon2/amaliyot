6f788632e77014d458f9a16562942e70b8b99350
abdulazizxon2 <boydadayevabbos@gmail.com>
2023-11-11 11:02:10 +0500

    kompaniya

---
+++ src/Redux/Reducer/KampaniyaRed.js
@@ -0,0 +1,8 @@
+let KampData={
+   
+}
+export default function Kapmaniya(state = KampData, { type, payload }) {
+    switch (type) {
+      default: return state
+    }
+  }
--- src/Redux/Reducer/RootRed.js
+++ src/Redux/Reducer/RootRed.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import NavbarRedux from "./NavbarRedux";
 import GlavniyRed from "./GlavniyRed";
 import DostavkaRed from "./DostavkaRed";
+import KapmaniyaRed from "./KampaniyaRed";
 const RootRedux = combineReducers({
     NavbarRedux,
     GlavniyRed,
-    DostavkaRed
+    DostavkaRed,
+    KapmaniyaRed
 });
 export default RootRedux;
