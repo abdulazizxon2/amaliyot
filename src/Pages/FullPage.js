eb39a6a4c2343b58a457dfce1447ebb8e0d74863
abdulazizxon2 <boydadayevabbos@gmail.com>
2023-12-21 13:07:29 +0300

    slider

--- src/Pages/FullPage.js
+++ src/Pages/FullPage.js
@@ -57,7 +57,7 @@ export default function FullPage() {
                 <img src={el} key={index}/>
               )
             })} */}
-        </SwiperSlide>
+        </SwiperSlide>git
       </Swiper>
       </div>
     </div>
